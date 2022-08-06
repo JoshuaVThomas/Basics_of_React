43890c6031dc69b21897592c843f7daa0168da68
JoshuaVThomas <joshuaquanticobusiness@gmail.com>
2022-08-06 18:54:47 +0530

    Edited

--- src/Planets.js
+++ src/Planets.js
@@ -1,7 +1,7 @@
 export const Planets = (props)=>{
       return(
         <div>
-          {props.isGas ? <h1>{props.name}</h1>:<h1></h1>}
+          {props.isGas && <h1>{props.name}</h1>}
         </div>
       )
     }
