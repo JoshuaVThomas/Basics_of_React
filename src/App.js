fbdaa86492f4748ecf8633c02be54cf7b211adb9
JoshuaVThomas <joshuaquanticobusiness@gmail.com>
2022-08-06 22:06:28 +0530

    Implemented useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  let [count , change] = useState(0);
+  let [input , changeInput] = useState("")
+  let [bool , showHide] = useState(false)
+ 
+  const twoWayBind = (event) => {
+    changeInput(input = event.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={()=>{change(count+=1)}}>Increment</button>
+      <button onClick={()=>{change(count-=1)}}>Decrement</button>
+      <button onClick={()=>{change(count=0)}}>Reset</button>
+      <hr></hr>
+      <input type="text" onChange={twoWayBind}></input>
+      <h3>{input}</h3>
+      <hr></hr>
+      <button onClick={()=>{showHide(!bool)}}>Show Text</button>
+      {bool && <p>Buzzinga</p>}
     </div>
   );
 }
